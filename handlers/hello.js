7b8db1ce18446b8dbf1945aa0b372ca178bf7833
Udit Narayan <info.udit90@gmail.com>
2021-04-30 23:40:07 +0530

    updates hello

--- handlers/hello.js
+++ handlers/hello.js
@@ -2,12 +2,12 @@ import handler from '../libs/handler';
 
 export const main = handler( async (event, context) => {
   return {
-    message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
+    message: `Hello! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
   };
 });
 
 const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
   setTimeout(() => {
     resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
+  }, time * 500)
 );
